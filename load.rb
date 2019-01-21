6dfbba75f556bb05c87525ee1077fe803b5fac34
Attiq Rao <attiqrao@Attiqs-MacBook-Pro.local>
2019-01-22 02:22:02 +0500

    generated errors and responses csv files

--- load.rb
+++ load.rb
@@ -3,6 +3,8 @@ Bundler.setup
 require 'savon'
 require "yaml"
 require 'roo'
+require 'csv'
+require 'nokogiri'
 
 class Load
 
@@ -18,21 +20,38 @@ class Load
     xlsx = Roo::Spreadsheet.open(@config["path_to_file"])
 
     xlsx.each_with_pagename do |name, sheet|
+      errors = []
+      infos = []
       sheet.each_row_streaming(pad_cells: true, offset: 1) do |row|
         next if row.eql?(sheet.first_row)
 
         # Post XML data to LeasePak API to save the model data
         response = @client.call(:load_mnf_mdl, message: { xmlDocument: generate_xml(row) })
 
-        # Store the response from LeasePak API in log file
-        File.open('log/responses.log','a') do |line|
-           line.puts "\r" + "Response for #{row[0]} from LeasePak: #{response}"
+        res = Nokogiri.XML(response.hash[:envelope][:body][:load_mnf_mdl_response][:return])
+
+        if res.search('ERROR').text.eql?("")
+          infos << [infos.size + 1, "#{row[2]}", res.search('INFO').text.split(".")[1]]
+        else
+          errors << [errors.size + 1, "#{row[2]}", res.search('ERROR').text]
         end
       end
+
+      # # Store the response from LeasePak API in log file
+      CSV.open("log/responses_#{Time.now}.csv", "wb") do |csv|
+        csv << ["No", "Name", "Response"]
+        infos.each {|info| csv << info}
+      end unless infos.empty?
+
+      CSV.open("log/errors_#{Time.now}.csv", "wb") do |csv|
+        csv << ["No", "Name", "Error"]
+        errors.each {|error| csv << error}
+      end unless errors.empty?
     end
+
   rescue Savon::Error => exception
     # Store message in log if client failes to connect with LeasePak API
-    File.open('log/errors.log','a') do |line|
+    File.open('log/connection_error.log', 'a') do |line|
        line.puts "\r" + "An error occurred while calling the LeasePak: #{exception.message}"
     end
   end
